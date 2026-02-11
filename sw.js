93574d65818b10790adc46d2c53aa84e8ae5417e
Mohamed Dah Agove <agwfmhmd@gmail.com>
2026-02-11 09:55:06 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mda-erp-v4'; // قمت بتغيير الاسم لفرض التحديث
+const CACHE_NAME = 'mda-erp-v5'; // قمت بتغيير الاسم لفرض التحديث
 
 self.addEventListener('install', (e) => {
   self.skipWaiting(); // هذا السطر يجبر المتصفح على تثبيت التحديث فوراً
